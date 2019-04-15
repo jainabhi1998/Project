6aad66f5cd41036d6438024224ee97f71b7d41ff
Abhishek Jain <jainabhi222.aj@gmail.com>
2019-04-15 09:43:54 +0530

    Update abhi1.c

--- abhi1.c
+++ abhi1.c
@@ -1,4 +1,4 @@
-s
+
 #include<iostream>
 using namespace std;
 
@@ -29,3 +29,50 @@ void Arrival_Time_Sorting()
 	}
 }
 
+int main()
+{
+	int i, time = 0, burst_time = 0, largest;
+	char c;
+        float wait_time = 0, turnaround_time = 0, average_waiting_time, average_turnaround_time;
+//	printf("\nEnter Total Number of Processes:\t");
+ //	scanf("%d", &limit);
+ 	limit=4;
+ 	
+ /*	for(i = 0, c = 'A'; i < limit; i++, c++)
+ 	{
+ 		process_queue[i].process_name = c;
+		printf("\nEnter Details For Process[%C]:\n", process_queue[i].process_name);
+		printf("Enter Arrival Time:\t");
+		scanf("%d", &process_queue[i].arrival_time );
+		printf("Enter Burst Time:\t");
+		scanf("%d", &process_queue[i].burst_time);
+		printf("Enter Priority:\t");
+		scanf("%d", &process_queue[i].priority);
+ 		process_queue[i].status = 0;
+ 		burst_time = burst_time + process_queue[i].burst_time;
+	}*/
+	process_queue[1].arrival_time=4;
+	process_queue[1].burst_time=2;
+	burst_time = burst_time + process_queue[1].burst_time;
+	process_queue[1].process_name='A';
+	process_queue[2].process_name='B';
+	process_queue[3].process_name='C';
+	process_queue[4].process_name='D';
+	process_queue[2].arrival_time=2;
+	process_queue[1].status = 0;
+	process_queue[2].status = 0;
+	process_queue[3].status = 0;
+	process_queue[4].status = 0;
+	
+	process_queue[2].burst_time=2;
+	burst_time = burst_time + process_queue[2].burst_time;
+	
+	process_queue[3].arrival_time=3;
+	process_queue[3].burst_time=3;
+	burst_time = burst_time + process_queue[3].burst_time;
+	
+	process_queue[4].arrival_time=4;
+	process_queue[4].burst_time=2;
+	burst_time = burst_time + process_queue[4].burst_time;
+	
+	
